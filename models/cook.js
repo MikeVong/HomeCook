63aa06e68fc78f4a30e9fbbdaf18bcf83072da4a
Mike Vong <mv910@hotmail.com>
2020-07-09 15:05:31 -0700

    added 'eater page' map function

--- models/cook.js
+++ models/cook.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
+const cookSchema = new Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
   dish: { type: String },
   ingredients: { type: String, required: true },
   coordinates: { type: Array},
-  address: { type: String}
-  /*,
-  cost: { type: String, required: true },
-  portions: { type: String, required: true },
-  email: { type: String, required: true },
+  address: { type: String},
+  cost: { type: String },
+  portions: { type: String },
+  email: { type: String },
+  src: {type: String}
   
-  */
 });
 
-const Cook = mongoose.model("Cook", bookSchema);
+const Cook = mongoose.model("Cook", cookSchema);
 
 module.exports = Cook;
